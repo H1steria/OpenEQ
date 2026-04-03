62fe29d2eab442111365d08cd12aabb53439ae94
Samuel Pontefract <zia32kvs@duck.com>
2026-04-03 12:43:18 +0200

    Bump version numbers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.turbofan3360.openeq"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.2.3"
+        versionCode = 10
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
